3e0560bd0fed69eae957651b61fbab0cc9a916e3
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-09-09 14:06:57 +0100

    Include testimage

--- adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/ImageType.kt
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/ImageType.kt
@@ -3,9 +3,9 @@ package net.adoptopenjdk.api.v3.models
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 
-@Schema(type = SchemaType.STRING, enumeration = ["jdk", "jre"], example = "jdk")
+@Schema(type = SchemaType.STRING, enumeration = ["jdk", "jre", "testimage"], example = "jdk")
 enum class ImageType : FileNameMatcher {
-    jdk, jre;
+    jdk, jre, testimage;
 
     override lateinit var names: List<String>
 
