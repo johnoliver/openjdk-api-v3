0dd3b2eb0934b4e2b37c6285de8574e3aa5f448d
John Oliver <1615532+johnoliver@users.noreply.github.com>
2021-08-04 10:13:18 +0000

    rename adoptium vendor to eclipse

--- adoptopenjdk-models-parent/adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/Vendor.kt
+++ adoptopenjdk-models-parent/adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/Vendor.kt
@@ -7,7 +7,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema
 */
 @Schema(ref = VendorSchemaRef.SCHEMA_REF)
 enum class Vendor {
-    adoptium, adoptopenjdk, openjdk, alibaba, ibm;
+    adoptium, adoptopenjdk, openjdk, alibaba, ibm, eclipse;
     // , amazon, azul, bellsoft, , sap;
 
     companion object {
