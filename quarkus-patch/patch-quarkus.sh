7c2cb88d4524b8e4b8827dc3f068267a2f206f95
John Oliver <1615532+johnoliver@users.noreply.github.com>
2021-08-09 14:13:30 +0000

    user curl

--- quarkus-patch/patch-quarkus.sh
+++ quarkus-patch/patch-quarkus.sh
@@ -2,7 +2,7 @@
 
 # Temporary hack until patch is release on main quarkus project
 
-wget "https://github.com/quarkusio/quarkus/archive/refs/tags/2.1.1.Final.zip"
+curl -sLo 2.1.1.Final.zip "https://github.com/quarkusio/quarkus/archive/refs/tags/2.1.1.Final.zip"
 unzip -q 2.1.1.Final.zip
 cd quarkus-2.1.1.Final/
 patch -p 1 < /tmp/patch
