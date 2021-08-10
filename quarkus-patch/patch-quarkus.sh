b1aeefe57becb049e9b4fdd80a381c36a53acc3c
John Oliver <1615532+johnoliver@users.noreply.github.com>
2021-08-10 13:01:39 +0000

    use docker

--- quarkus-patch/patch-quarkus.sh
+++ quarkus-patch/patch-quarkus.sh
@@ -2,9 +2,10 @@
 
 # Temporary hack until patch is release on main quarkus project
 
-curl -sLo 2.1.1.Final.zip "https://github.com/quarkusio/quarkus/archive/refs/tags/2.1.1.Final.zip"
-unzip -q 2.1.1.Final.zip
-cd quarkus-2.1.1.Final/
-patch -p 1 < ../release.patch
+git clone https://github.com/quarkusio/quarkus.git
+cd quarkus
+git fetch origin 2.1.1.Final
+git checkout 2.1.1.Final
+git apply ../release.patch
 cd independent-projects/resteasy-reactive/server/vertx
 ../../../../mvnw clean install
